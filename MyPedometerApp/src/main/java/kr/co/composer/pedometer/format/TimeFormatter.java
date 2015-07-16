a72161d1abd583f2ae50e8e2a29bf4238a9543e2
composer10 <composer10@naver.com>
2015-07-16 23:51:17 +0900

    0716 sql

--- MyPedometerApp/src/main/java/kr/co/composer/pedometer/format/TimeFormatter.java
+++ MyPedometerApp/src/main/java/kr/co/composer/pedometer/format/TimeFormatter.java
@@ -1,8 +1,8 @@
 package kr.co.composer.pedometer.format;
 
 public class TimeFormatter {
-//	public static final String TIME_FORMAT = "%02d:%02d:%02d";
-	public static final String TIME_FORMAT = "HH:mm:ss";
+	//	public static final String TIME_FORMAT = "%02d:%02d:%02d";
+	public static final String TIME_FORMAT = "h:mm a";
+	public static final String DATE_FORMAT = "yyyy-MM-dd";
 	public static final String START_DATE_FORMAT = "yyyy-MM-dd kk:mm:ss";
-	
 }
