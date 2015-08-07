6813c587be1b13c11e6482085920fb7d93e6a0b2
composer10 <composer10@naver.com>
2015-08-07 22:45:10 +0900

    0807 development mypedometer complete

--- MyPedometerApp/src/main/java/kr/co/composer/pedometer/activity/fragment/HistoryFragment.java
+++ MyPedometerApp/src/main/java/kr/co/composer/pedometer/activity/fragment/HistoryFragment.java
@@ -1,7 +1,6 @@
 package kr.co.composer.pedometer.activity.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,7 +31,6 @@ public class HistoryFragment extends Fragment{
         pedoHistoryBO = new PedoHistoryBO();
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history,container,false);
